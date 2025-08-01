20eb2a5a692aa8f60cf39e2fed969578cb62f228
flofeyka <danilbashirov0@gmail.com>
2025-08-01 10:13:09 +0500

    Socket optimized. Dublicate code partially fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RoomModule } from './room/room.module';
+import { AppService } from './app.service';
 
 @Module({
   imports: [RoomModule],
   controllers: [],
-  providers: [],
+  providers: [AppService],
+  exports: [AppService]
 })
-export class AppModule {}
+export class AppModule { }
--- src/room/room.module.ts
+++ src/room/room.module.ts
@@ -1,13 +1,15 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { RoomController } from './room.controller';
 import { RoomGateway } from './room.gateway';
 import { RoomService } from './room.service';
 import { RoomPersistenceService } from './room-persistence.service';
+import { AppModule } from 'src/app.module';
+import { AuthRoomGuard } from './auth-room.guard';
 
 @Module({
-  imports: [PrismaModule],
-  providers: [RoomService, RoomGateway, RoomPersistenceService],
+  imports: [PrismaModule, forwardRef(() => AppModule)],
+  providers: [RoomService, RoomGateway, RoomPersistenceService, AuthRoomGuard],
   controllers: [RoomController],
 })
-export class RoomModule {}
+export class RoomModule { }
